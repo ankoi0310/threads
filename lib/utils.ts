df4a5c08e05bc61b09b9b90e68432fe771976f71
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-08-15 02:25:13 +0700

    test timezone

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,31 +1,45 @@
 import { formatInTimeZone } from 'date-fns-tz'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 // created by chatgpt
 export const isBase64Image = (imageData: string) => {
-  const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
-  return base64Regex.test(imageData);
+  const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/
+  return base64Regex.test(imageData)
 }
 
 // created by chatgpt
 export const formatDateString = (dateString: string) => {
-  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  console.log(timeZone)
-  return formatInTimeZone(dateString, timeZone, 'HH:mm - dd MMM yyyy');
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    timeZone: 'Asia/SaiGon',
+  }
+
+  const date = new Date(dateString)
+  const formattedDate = date.toLocaleDateString([], options)
+
+  const time = date.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'Asia/SaiGon',
+  })
+
+  return `${time} - ${formattedDate}`
 }
 
 // created by chatgpt
 export const formatThreadCount = (count: number) => {
   if (count === 0) {
-    return 'No Threads';
+    return 'No Threads'
   } else {
-    const threadCount = count.toString().padStart(2, '0');
-    const threadWord = count === 1 ? 'Thread' : 'Threads';
-    return `${threadCount} ${threadWord}`;
+    const threadCount = count.toString().padStart(2, '0')
+    const threadWord = count === 1 ? 'Thread' : 'Threads'
+    return `${threadCount} ${threadWord}`
   }
 }
